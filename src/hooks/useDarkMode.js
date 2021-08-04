01fe9b369a448d9cb90f9b1cff8a6fa0885f5c69
Kenny Miesner <84544990+kennymiesner@users.noreply.github.com>
2021-08-04 18:22:02 -0500

    Dark mode functionality complete

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,5 +1,6 @@
-
+import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = () => {
-
-}
+  const [darkMode, setDarkMode] = useLocalStorage("Dark Mode");
+  return [darkMode, setDarkMode];
+};
